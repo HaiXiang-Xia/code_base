3243d79dd864fffdc24ccd3752dd05b7b60e6eeb
HaiXiang-Xia <118971342+HaiXiang-Xia@users.noreply.github.com>
2022-11-26 22:11:46 +0800

    Update bilibili_video.py

--- bilibili_video.py
+++ bilibili_video.py
@@ -11,7 +11,7 @@ import re
 
 headers = {
     'referer': 'https://www.bilibili.com/',
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
+    'user-agent': '',
 }
 
 #视频，音频 合并
